b60003c811b410b17b658c1c815e075d8ddaf83e
sam88mohd <sam88mohd@gmail.com>
2021-10-05 22:09:23 +0800

    change style for qualification

--- pages/components/Banner.js
+++ pages/components/Banner.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { FaFacebookF, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import styles from "../../styles/Banner.module.css";
 
 const Banner = () => {
